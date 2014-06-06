bdb8697e1a0c14b1f6f42355d1870b7694102c93
Adam Shinn <adam.b.shinn@gmail.com>
2014-06-06 15:26:23 -0700

    latest savetonumpy

--- savetonumpy.py
+++ savetonumpy.py
@@ -7,38 +7,32 @@ import sys
 
 training = pd.read_csv("data/training.csv", index_col = 0)
 test = pd.read_csv("data/test.csv", index_col = 0)
-# testdf = pd.read_csv("test.csv")
 
-# print("sample size: {}".format(len(traindf)))
-# sampleindex = traindf.pop(u"Unnamed: 0")
-
-# cpubusy = traindf.pop("cpu_01_busy")
-# y = np.mat(cpubusy.values).T
+print 0, len(training.columns)
 
 #
 # work on features
 #
 lab_train = training.pop("Label")
+weight = training.pop("Weight")
 
-pdb.set_trace()
+print 1, len(training.columns)
+print("Feature matrix columns:")
+for col in training.columns:
+    print u"  " + col
 
 lab_train.iloc[np.where(lab_train == "s")] = 1
 lab_train.iloc[np.where(lab_train == "b")] = 0
 
 y_train = lab_train.values
-# m = y.shape[0]
+weight = weight.values
 
-# turn time into integer 
-# dttime = pd.to_datetime(traindf.pop("sample_time"))
-# inttime = dttime.astype(np.int64)
-
-# del traindf["syst_page_read_ipo_rate"]
-# del traindf["page_global_valid_fault_rate"]
-
-# X = np.mat(np.c_[np.ones(m), traindf.values, m_id_dummy, inttime])
 X_train = training.values
 X_test = test.values
-# X = np.mat(np.c_[training.values])
+
+print 2, X_train.shape
+
+# pdb.set_trace()
 
 X_train = X_train.astype(np.float64)
 X_test = X_test.astype(np.float64)
@@ -47,16 +41,25 @@ y_train = y_train.astype(np.float64)
 # print("X_train shape: {}".format(X_train.shape))
 # print("Y_train shape: {}".format(y_train.shape))
 
+print("X_train shape: {}".format(X_train.shape))
+print("Y_train shape: {}".format(y_train.shape))
+print("Weight shape: {}".format(weight.shape))
+
+
 if __name__ == "__main__":
     if len(sys.argv) == 2 and sys.argv[1] == "-s":
         np.save("X_train.npy", X_train)
         np.save("Y_train.npy", y_train)
-        print("Output written:\nX_train.npy\nY_train.npy")
+        print("Output written: X_train.npy, Y_train.npy")
         print("X_train shape: {}".format(X_train.shape))
         print("Y_train shape: {}".format(y_train.shape))
 
         np.save("X_test.npy", X_test)
-        print("\nX_test.npy")
+        print("Output written: X_test.npy")
         print("X_test shape: {}".format(X_test.shape))
+
+        np.save("weight.npy", weight)
+        print("Output written: weight.npy")
+        print("Weight shape: {}".format(weight.shape))
     else:
-        print("no output")
+        print("---> No output, use -s flag to save")
